aac10beb379c97006b3e4d17584bd3208e35ef81
Alex <kadissa70@gmail.com>
2021-04-15 18:40:29 +0300

    добавлено редактирование профиля

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from datetime import datetime
 from werkzeug.urls import url_parse
 from app import app, db
-from app.forms import LoginForm, RegistrationForm
+from app.forms import LoginForm, RegistrationForm, EditProfile
 from app.models import User
 
 
@@ -62,11 +62,13 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.before_request
 def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+
 
 @app.route('/user/<username>')
 @login_required
@@ -78,3 +80,20 @@ def user(username):
         {'author': user, 'body': 'Проверка'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['get', 'post'])
+@login_required
+def edit_profile():
+    form = EditProfile()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes have been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form, title='Edit '
+                                                                 'Profile')
